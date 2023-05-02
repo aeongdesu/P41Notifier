807f2b0053e3ca1773b31c5b26d6d965a5f65b5a
aeongdesu <osu@kakao.com>
2023-05-02 09:05:54 +0000

    1.0.0

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,9 +1,22 @@
 import fetch from "node-fetch"
 
-const PREVIEW_URL = "https://t.me/s/hotdeal_kr"
+const PREVIEW_URL = "https://t.me/s/hotdeal_kr?q=p41+1tb"
+
+// horror
+const lastHotdeal = /<a class="tgme_widget_message_inline_button url_button" href="(.*)<\/span>/g
+const buttonLink = /href="([^"]+)/
+const buttonText = /<span[^>]*>(.*?)<\/span>/
+const source = /\((.*?)\)/
 
 export default async () => {
     const preview = await fetch(PREVIEW_URL)
     if (!preview.ok) return
-    return preview.json()
+    const text = await preview.text()
+    const links = text.match(lastHotdeal)
+    if (!links) return
+    const link = links[links.length - 1]
+    return {
+        link: link.match(buttonLink)[1].replace(/&amp;/g, "&"),
+        source: (link.match(buttonText)[1]).match(source)[1]
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
+import * as dotenv from "dotenv"
+const env = dotenv.config()
+
 import express from "express"
+const app = express()
+app.use(express.json()) // for cron
 
 import FetchData from "./FetchData.js"
 
-const app = express()
+import { Deta } from "deta"
+const deta = new Deta()
+const db = deta.Base("P41Notifier")
 
 const port = process.env.PORT
-const webhook = process.env.DISCORD_WEBHOOK
+const webhook_url = process.env.DISCORD_WEBHOOK || env.parsed?.DISCORD_WEBHOOK
+const mention = process.env.MENTION_ID || env.parsed?.MENTION_ID
 
 app.get("/", (req, res) => {
-    res.redirect("https://github.com/aeongdesu/P41Notifier")
+    res.send({ repo: "https://github.com/aeongdesu/P41Notifier", path: "/latest" })
 })
 
-app.get("/notify", async (req, res) => {
-    if (!webhook) return res.status(404).send("No webhook provided")
+app.get("/latest", async (req, res) => {
+    const latest = await db.get("latest")
+    return res.send(latest)
 })
 
 // cron
 app.post("/__space/v0/actions", async (req, res) => {
-    console.log(req)
-    const event = req.body.event
-    if (event.id === "fetch") {
-        res.send(await FetchData())
-    }
-    res.sendStatus(200)
+    const event = req.body?.event
+    if (event?.id !== "fetch") return res.sendStatus(403)
+
+    const data = await FetchData()
+    let latest = await db.get("latest")
+    if (latest?.link == data.link) return res.send("Up to date")
+
+    latest = await db.put(data, "latest")
+
+    const webhook = await fetch(webhook_url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            content: `<@${mention}>\n${data.link}`
+        })
+    })
+    if (!webhook.ok) return res.sendStatus(500)
+    return res.send(latest)
 })
 
 app.listen(port, () => {
